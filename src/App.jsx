98542f51bddf85dc9f38de917cc74c4e2f299d2d
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2024-09-22 23:16:03 +0100

    Added Dev Images

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,9 @@ import Register from "./pages/Register";
 import Inventory from "./pages/Admin/Products/Inventory";
 import NotFound from "./pages/NotFound";
 import AdminDashboard from "./pages/Admin/AdminDashboard";
+import ForgetPassword from "./pages/ForgetPassword";
+import ResetPassword from "./pages/ResetPassword";
+import ResendVerification from "./pages/ResendVerification";
 
 function App() {
   return (
@@ -21,54 +24,31 @@ function App() {
         <Route element={<Public />}>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/sign-up" element={<Register />} />
+          <Route path="/forget-password" element={<ForgetPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route
+            path="/resend-verification-link"
+            element={<ResendVerification />}
+          />
 
           {/* <Route path="/check-email" element={<CheckEmail />} />
-            <Route path="/password-reset" element={<PasswordReset />} />
             <Route path="/set-new-password" element={<ResetPassword />} />
-            <Route path="/forget-password" element={<ForgetPassword />} />
-            <Route path="/ui" element={<UI />} /> */}
+            
+            */}
         </Route>
+
         <Route element={<Private />}>
-          <Route element={<MainDashboardLayout />}>
-            <Route path="/dashboard" element={<AdminDashboard />}>
+          <Route path="admin" element={<MainDashboardLayout />}>
+            <Route path="dashboard" element={<AdminDashboard />}>
               {/* <Route index element={<Navigate replace to="dashboard" />} /> */}
               {/* <Route path="/dashboard/home-admin" element={<Dashboard />} />
               <Route path="/dashboard/order-home" element={<Dashboard />} /> */}
             </Route>
-            <Route path="/orders-management" element={<NotFound />}>
-              <Route
-                path="/orders-management/orders-list"
-                element={<NotFound />}
-              />
-              <Route
-                path="/orders-management/product-reviews"
-                element={<NotFound />}
-              />
-              <Route
-                path="/orders-management/abandoned-cart"
-                element={<NotFound />}
-              />
-              <Route
-                path="/orders-management/order-cancellation-requests"
-                element={<NotFound />}
-              />
-              <Route
-                path="/orders-management/order-return-requests"
-                element={<NotFound />}
-              />
-              <Route
-                path="/orders-management/wishlist-mgt"
-                element={<NotFound />}
-              />
-              <Route
-                path="/orders-management/coupon-managements"
-                element={<NotFound />}
-              />
-            </Route>
-            <Route path="/product" element={<Inventory />}>
+
+            <Route path="product" element={<Inventory />}>
               {/* <Route index element={<Navigate replace to="dashboard" />} /> */}
-              <Route path="/product/inventory" element={<Inventory />} />
+              <Route path="product/inventory" element={<Inventory />} />
               {/* <Route path="/dashboard/order-home" element={<Dashboard />} /> */}
             </Route>
           </Route>
