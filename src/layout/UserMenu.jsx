f3d6f33a20e50e663a04904cbc9e0088734d94df
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2024-09-27 00:42:55 +0100

    Completed auth features

--- src/layout/UserMenu.jsx
+++ src/layout/UserMenu.jsx
@@ -1,24 +1,28 @@
 import { Link } from "react-router-dom";
 import userMenu from "src/data/userMenu";
-// import { useSelector } from "react-redux";
-// import { RootState } from "src/functions/Redux/rootReducer";
-// import { useDispatch } from "react-redux";
-// import { logoutThunk } from "src/functions/Redux/auth/authThunk";
+import { useSelector } from "react-redux";
+
+import { useDispatch } from "react-redux";
+import { logoutThunk } from "src/Redux/auth/authThunk";
 
 const UserMenu = () => {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  // const handleLogout = () => {
-  //   dispatch(logoutThunk());
-  // };
-  // const { currentUser } = useSelector((state: RootState) => state.auth);
+  const handleLogout = () => {
+    dispatch(logoutThunk());
+  };
+  const { currentUser } = useSelector((state) => state.auth);
+
+  // console.log("currentUser", currentUser);
   return (
     <div className="absolute top-10 right-3 transition-all duration-300 ease-in">
       <div className="z-50 my-4 text-base text-left list-none bg-gray-50  rounded shadow ">
         <div className="border-b pb-2 border-gray-400 p-4 px-8">
-          <p className=" text-gray-900 font-semibold capitalize">Diy Admin</p>
+          <p className=" text-gray-900 font-semibold capitalize">
+            {currentUser?.first_name} {currentUser?.last_name}
+          </p>
           <p className="text-sm font-normal text-gray-500 truncate ">
-            Admin@mrdiy.com
+            {currentUser?.email}
           </p>
         </div>
         <ul className="py-2 ">
@@ -34,7 +38,7 @@ const UserMenu = () => {
           ))}
           <li
             className="block  py-2 px-8 text-sm text-gray-800 hover:text-gray-900 hover:bg-gray-200 cursor-pointer"
-            // onClick={handleLogout}
+            onClick={handleLogout}
           >
             Sign out
           </li>
