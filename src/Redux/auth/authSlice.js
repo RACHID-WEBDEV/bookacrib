65ce1ed61dc26dbcf690969d6cdf32d695fa17ce
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2024-09-27 00:43:34 +0100

    Completed auth features

--- src/Redux/auth/authSlice.js
+++ src/Redux/auth/authSlice.js
@@ -1,14 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { loginThunk, logoutThunk } from "./authThunk";
+import {
+  loginThunk,
+  logoutThunk,
+  signupThunk,
+  forgetPasswordThunk,
+  resendVerificationThunk,
+  accountVerificationThunk,
+  resetPasswordThunk,
+} from "./authThunk";
 import Cookies from "js-cookie";
 
 const initialState = {
-  currentUser: Cookies.get("currentUser")
-    ? JSON.parse(Cookies.get("currentUser"))
+  currentUser: Cookies.get("bookacrib_currentUser")
+    ? JSON.parse(Cookies.get("bookacrib_currentUser"))
     : null,
-  isAuthenticated: !!Cookies.get("admin_token"),
+  isAuthenticated: !!Cookies.get("bookacrib_admin_token"),
   loading: false,
   error: null,
+  registerUser: [],
+  forgetpassword: [],
+  resendverification: [],
+  accountverification: [],
+  passwordreset: [],
 };
 
 const authSlice = createSlice({
@@ -31,6 +44,75 @@ const authSlice = createSlice({
         state.loading = false;
         state.isAuthenticated = false;
         state.error = action.payload; // Set error state from rejection value
+      })
+
+      .addCase(signupThunk.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(signupThunk.fulfilled, (state, action) => {
+        state.loading = false;
+        state.registerUser = [...state.registerUser, action.payload];
+      })
+      .addCase(signupThunk.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload ?? "Failed to add register user";
+      })
+      .addCase(forgetPasswordThunk.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(forgetPasswordThunk.fulfilled, (state, action) => {
+        state.loading = false;
+        state.forgetpassword = [...state.forgetpassword, action.payload];
+      })
+      .addCase(forgetPasswordThunk.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload ?? "Failed to reset forget password";
+      })
+
+      .addCase(resendVerificationThunk.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(resendVerificationThunk.fulfilled, (state, action) => {
+        state.loading = false;
+        state.resendverification = [
+          ...state.resendverification,
+          action.payload,
+        ];
+      })
+      .addCase(resendVerificationThunk.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload ?? "Failed to resend verification token";
+      })
+      .addCase(accountVerificationThunk.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(accountVerificationThunk.fulfilled, (state, action) => {
+        state.loading = false;
+        state.accountverification = [
+          ...state.accountverification,
+          action.payload,
+        ];
+      })
+      .addCase(accountVerificationThunk.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload ?? "Failed to verification account";
+      })
+
+      .addCase(resetPasswordThunk.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(resetPasswordThunk.fulfilled, (state, action) => {
+        state.loading = false;
+        state.passwordreset = [...state.passwordreset, action.payload];
+      })
+      .addCase(resetPasswordThunk.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload ?? "Failed to verification account";
       })
       .addCase(logoutThunk.pending, (state) => {
         state.loading = true;
