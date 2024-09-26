ea72ddb4e346668e4daec1d829ac280b4bf40e46
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2024-09-27 00:43:40 +0100

    Completed auth features

--- src/Redux/auth/authThunk.js
+++ src/Redux/auth/authThunk.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import Cookies from "js-cookie";
 import toast from "react-hot-toast";
-import { postData } from "../../utils/api";
+import { patchData, postData } from "../../utils/api";
 
 // Async thunk action creator for login
 const loginThunk = createAsyncThunk(
@@ -9,45 +9,48 @@ const loginThunk = createAsyncThunk(
   async (userData, { rejectWithValue }) => {
     try {
       // Make an HTTP POST request to the login endpoint with user data
-      const response = await postData("admin/auth", userData);
+      const response = await postData(
+        "/v1/users/login?with[]=companies",
+        userData
+      );
 
       // Assert the type of 'response' to 'LoginResponse'
       const loginResponse = response;
       console.log("loginResponse", loginResponse);
 
-      if (loginResponse?.status === 202) {
+      if (loginResponse?.status === 200) {
         toast.success(loginResponse?.message);
       }
 
       // Extract user data and access token from the response
       // eslint-disable-next-line no-unsafe-optional-chaining
-      const { user, token } = loginResponse?.data;
+      const { access_token } = loginResponse?.data;
 
       // console.log(user);
       // console.log(access_token);
 
       // Store the access token in a cookie with expiry, sameSite, and secure options
-      Cookies.set("admin_token", token, {
+      Cookies.set("bookacrib_admin_token", access_token, {
         expires: 7,
         sameSite: "None",
         secure: true,
       });
 
       // Store user information in a cookie
-      const userInfo = JSON.stringify(user);
-      Cookies.set("currentUser", userInfo, {
+      const userInfo = JSON.stringify(loginResponse?.data);
+      Cookies.set("bookacrib_currentUser", userInfo, {
         expires: 7,
         sameSite: "None",
         secure: true,
       });
 
       // Return the user data
-      return user;
+      return loginResponse?.data;
     } catch (error) {
       // Handle errors
       const errorMessage = error.response.data.message;
 
-      console.log("ererer", error);
+      console.log("error", error);
       if (
         error.status === 400 ||
         error.status === 401 ||
@@ -67,25 +70,19 @@ const loginThunk = createAsyncThunk(
 const logoutThunk = createAsyncThunk("auth/logout", async (data, thunkAPI) => {
   try {
     // Make an HTTP POST request to the logout endpoint
-    const response = await postData("auth/logout", "");
+    const response = await postData("/v1/users/logout", "");
 
     // Remove access token and user cookies
-    Cookies.remove("admin_token");
-    Cookies.remove("currentUser");
+    Cookies.remove("bookacrib_admin_token");
+    Cookies.remove("bookacrib_currentUser");
 
     // Return success message
     // return "Logged out successfully";
     console.log("Logged out successfully");
 
-    if (
-      response?.status === 202 ||
-      response?.status === 200 ||
-      response?.status === 201 ||
-      response?.status_code === 202 ||
-      response?.status_code === 200 ||
-      response?.status_code === 201
-    ) {
-      toast.success("Logged out successfully");
+    const { status, message } = response;
+    if (status >= 200 && status < 300) {
+      toast.success(message);
     }
     return response;
   } catch (error) {
@@ -96,4 +93,115 @@ const logoutThunk = createAsyncThunk("auth/logout", async (data, thunkAPI) => {
   }
 });
 
-export { loginThunk, logoutThunk };
+const signupThunk = createAsyncThunk(
+  "auth/signup",
+  async (userData, { rejectWithValue }) => {
+    try {
+      const response = await postData("/v1/users/registration", userData);
+      const { status, message } = response;
+      if (status >= 200 && status < 300) {
+        toast.success(message);
+      }
+
+      return response;
+    } catch (error) {
+      const errorMessage = error.response?.data || error.message;
+      console.error(errorMessage);
+      return rejectWithValue(errorMessage);
+    }
+  }
+);
+
+const forgetPasswordThunk = createAsyncThunk(
+  "auth/forgetPassword",
+  async (userEmail, { rejectWithValue }) => {
+    try {
+      const response = await postData("/v1/users/forgot-password", userEmail);
+      const { status, message } = response;
+      if (status >= 200 && status < 300) {
+        toast.success(message);
+      }
+
+      return response;
+    } catch (error) {
+      const errorMessage = error.response?.data || error.message;
+      console.error(errorMessage);
+      return rejectWithValue(errorMessage);
+    }
+  }
+);
+
+const resendVerificationThunk = createAsyncThunk(
+  "auth/resendVerification",
+  async (userEmail, { rejectWithValue }) => {
+    try {
+      const response = await postData(
+        "/v1/users/resend-verification-link",
+        userEmail
+      );
+      const { status, message } = response;
+      if (status >= 200 && status < 300) {
+        toast.success(message);
+      }
+
+      return response;
+    } catch (error) {
+      const errorMessage = error.response?.data || error.message;
+      console.error(errorMessage);
+      return rejectWithValue(errorMessage);
+    }
+  }
+);
+const accountVerificationThunk = createAsyncThunk(
+  "auth/accountVerification",
+  async (userEmail_Token, { rejectWithValue }) => {
+    try {
+      const response = await patchData(
+        "/v1/users/account-verification",
+        userEmail_Token
+      );
+      const { status, message } = response;
+      if (status >= 200 && status < 300) {
+        toast.success(message);
+      }
+
+      return response;
+    } catch (error) {
+      const errorMessage = error.response?.data || error.message;
+      console.error(errorMessage);
+      return rejectWithValue(errorMessage);
+    }
+  }
+);
+
+const resetPasswordThunk = createAsyncThunk(
+  "auth/resetPassword",
+  async (userPassword_Token, { rejectWithValue }) => {
+    try {
+      const response = await patchData(
+        "/v1/users/reset-password",
+        userPassword_Token
+      );
+      const { status, message } = response;
+      if (status >= 200 && status < 300) {
+        toast.success(message);
+      }
+
+      return response;
+    } catch (error) {
+      const errorMessage = error.response?.data || error.message;
+      console.error(errorMessage);
+      return rejectWithValue(errorMessage);
+    }
+  }
+);
+
+export {
+  loginThunk,
+  logoutThunk,
+  signupThunk,
+  forgetPasswordThunk,
+  resendVerificationThunk,
+  accountVerificationThunk,
+  resetPasswordThunk,
+};
