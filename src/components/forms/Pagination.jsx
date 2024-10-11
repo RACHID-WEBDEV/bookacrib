84664b17b66d2aaac6c41c7f1713ab0b1f4d127b
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2024-10-11 23:38:16 +0100

    Pagination Modify

--- src/components/forms/Pagination.jsx
+++ src/components/forms/Pagination.jsx
@@ -4,18 +4,18 @@ const Pagination = ({ meta, handlePaginate }) => {
   // const dispatch = useDispatch();
   // console.log("metadata", meta);
   return (
-    <div className="flex items-center justify-between mt-3 mb-2">
-      <div className="text-gray-500">
+    <div className="flex items-center justify-center gap-4 lg:justify-between flex-wrap mt-3 mb-2">
+      <div className="text-gray-500 text-sm">
         Showing {meta?.from} to {meta?.to} out of {meta?.total}
       </div>
-      <div className="flex items-center gap-2">
+      <div className="flex items-center flex-wrap gap-1.5">
         {meta?.links &&
           meta.links.map((link, index) => (
             <button
-              className={` border-gray-400 p-0.5 px-2 ${
+              className={` border-gray-400 p-0.5 px-1.5 ${
                 link.active
-                  ? "bg-dark-100  text-white border-none size-9 rounded-full"
-                  : "bg-white  text-black"
+                  ? "bg-dark-100  text-white border-none size-8 rounded-full"
+                  : "bg-white  text-black text-sm"
               }  `}
               key={index}
               onClick={() => handlePaginate(link.url)} // Call the handlePaginate function when button is clicked
