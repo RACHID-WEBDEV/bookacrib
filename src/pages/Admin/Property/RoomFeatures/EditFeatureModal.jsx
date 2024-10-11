72d48f8cb4412b147c882bee393a525c44d1023e
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2024-10-11 20:37:36 +0100

    Bookcrib features

---
+++ src/pages/Admin/Property/RoomFeatures/EditFeatureModal.jsx
@@ -0,0 +1,155 @@
+/* eslint-disable react/prop-types */
+import { ScrollArea } from "../../../../components/forms/scroll-area";
+import HookForm from "../../../../components/forms/Form";
+import { Button } from "../../../../components/forms/Button";
+import { Input } from "../../../../components/forms/Input";
+// import { TextArea } from "../../../../components/forms/TextArea";
+
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import {
+  updateFeature,
+  fetchFeatures,
+} from "../../../../Redux/features/featuresThunk";
+import { FeatureSchema } from "../../../../schema/authSchema";
+import toast from "react-hot-toast";
+
+const EditFeatureModal = ({ setOpenModal, featureData }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  //  console.log("permissions", permissions);
+  const defaultFormValue = {
+    name: featureData?.name,
+    price: featureData?.price,
+  };
+  const onSubmit = async (data) => {
+    const filteredFormData = {
+      name: data?.name,
+      price: data?.price,
+    };
+
+    console.log("filtered data", filteredFormData);
+
+    // alert(JSON.stringify(filteredFormData));
+
+    try {
+      const result = await dispatch(
+        updateFeature({
+          id: featureData?.uuid,
+          formData: filteredFormData,
+        })
+      ).unwrap();
+      // console.log("Add result", result);
+
+      if (result.status >= 200 && result.status <= 300) {
+        dispatch(
+          fetchFeatures("/bookacrib-api-routes/v1/features/list-features")
+        );
+        toast.success(result.message, { duration: 6000 });
+        navigate("/admin/property/features");
+      }
+    } catch (error) {
+      // console.log("addNew Feature error", error);
+      if (error?.status >= 400 && error?.status <= 499) {
+        dispatch(
+          fetchFeatures("/bookacrib-api-routes/v1/features/list-features")
+        );
+        const errorMessage = Object.values(error?.errors).flat().join(", ");
+        if (error?.errors) {
+          toast.error(errorMessage, { duration: 6000 });
+        } else {
+          toast.error(error.message, { duration: 6000 });
+        }
+      }
+    }
+    setOpenModal(false);
+  };
+
+  return (
+    <div className="w-full h-full fixed inset-0 bg-black/70 z-50 bg-opacity-50 backdrop-blur">
+      <div
+        aria-hidden="true"
+        className=" overflow-y-hidden overflow-x-hidden z-50 flex justify-center items-center w-full  h-[calc(100%-1rem)] max-h-full "
+      >
+        <div className="relative p-4 w-full max-w-2xl max-h-full">
+          <HookForm
+            defaultValues={defaultFormValue}
+            onSubmit={onSubmit}
+            schema={FeatureSchema}
+          >
+            <div className="relative bg-white rounded-2xl shadow ">
+              <div className="flex items-center justify-between p-4 md:px-5 border-b rounded-t ">
+                <div>
+                  <h1 className="text-lg font-semibold text-gray-900 ">
+                    Update Feature
+                  </h1>
+                  <p className="text-gray-600 text-sm font-light">
+                    Update a room feature for this platform
+                  </p>
+                </div>
+                <button
+                  type="button"
+                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center "
+                  onClick={() => setOpenModal(false)}
+                >
+                  <svg
+                    className="w-3 h-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 14"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                    />
+                  </svg>
+                  <span className="sr-only">Close modal</span>
+                </button>
+              </div>
+              {/* contents below */}
+              <div className=" max-h-[560px] overflow-y-scroll">
+                <ScrollArea className="h-full">
+                  <div className="p-5">
+                    <div className="pr-6">
+                      <div className="lg:w-full">
+                        <Input
+                          name="name"
+                          label="Feature name"
+                          placeholder="Enter a name for the feature"
+                        />
+
+                        <Input
+                          name="price"
+                          label="Price"
+                          placeholder="Enter a price for the feature"
+                          type="number"
+                        />
+                      </div>
+                    </div>
+                    <hr />
+                  </div>
+                </ScrollArea>
+              </div>
+
+              <div className="p-4 border-t">
+                <div className="flex items-center gap-2 justify-end">
+                  <Button type="submit" size="xs">
+                    Update Feature
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </HookForm>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditFeatureModal;
---
+++ src/pages/Admin/Property/RoomFeatures/RoomFeatures.jsx
@@ -0,0 +1,314 @@
+import { useDispatch, useSelector } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+import {
+  deleteFeature,
+  fetchFeature,
+  fetchFeatures,
+} from "../../../../Redux/features/featuresThunk";
+import { useEffect, useState } from "react";
+import { Button } from "../../../../components/forms/Button";
+import LoadingSpinner from "../../../../components/Loading/LoadingSpinner";
+import {
+  DeleteIcon,
+  EditIcon,
+  EyeIconBold,
+  PlusIcon,
+} from "../../../../assets/SvgIcons";
+import ErrorStatus from "../../../../components/forms/ErrorStatus";
+import EditFeatureModal from "./EditFeatureModal";
+import AddNewFeatureModal from "./AddNewFeatureModal";
+// import { getLastSegmentFromUrl } from "../../../../utils/segmentSpliter";
+import Pagination from "../../../../components/forms/Pagination";
+import { Badge } from "../../../../components/forms/Badge";
+import ViewFeatureModal from "./ViewFeatureModal";
+import { formatNumber } from "../../../../lib/constants";
+import Notification from "../../../../components/shared/notification/Notification";
+import toast from "react-hot-toast";
+
+const RoomFeatures = () => {
+  const dispatch = useDispatch();
+  //   const navigate = useNavigate();
+
+  const { features, feature, loading, error } = useSelector(
+    (state) => state.features
+  );
+  const fetchFeatureHandler = (url) => {
+    dispatch(fetchFeatures(url));
+  };
+
+  useEffect(() => {
+    fetchFeatureHandler(
+      "bookacrib-api-routes/v1/features/list-features?limit=10"
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dispatch]);
+
+  const handlePaginate = (url) => {
+    if (url) {
+      fetchFeatureHandler(url);
+    }
+  };
+  // console.log("roles", roles);
+  const [openRoleModal, setOpenRoleModal] = useState(false);
+  const [editSupportModal, setEditSupportModal] = useState(false);
+  const [viewSupportModal, setViewSupportModal] = useState(false);
+  const [showDeletePopup, setShowDeletePopup] = useState(false);
+
+  const fetchSingleRole = (cat_id) => {
+    dispatch(fetchFeature(cat_id));
+    setEditSupportModal(true);
+  };
+
+  const fetchViewRole = (cat_id) => {
+    dispatch(fetchFeature(cat_id));
+    setViewSupportModal(true);
+  };
+
+  const deleteFeatureData = (cat_id) => {
+    dispatch(fetchFeature(cat_id));
+    setShowDeletePopup(true);
+  };
+
+  const handleFeatureDelete = async (feature) => {
+    // console.log("feature is here", feature);
+    if (feature) {
+      // Handle delete Feature logic here
+      try {
+        // setLoading(true);
+        const result = await dispatch(deleteFeature(feature?.uuid)).unwrap();
+        console.log("delete feature:", result);
+        if (
+          (result?.status >= 200 && result?.status < 300) ||
+          (result?.status_code >= 200 && result?.status_code)
+        ) {
+          //   navigate("/admin/property/features");
+          toast.success(result?.message);
+          dispatch(
+            fetchFeatures("bookacrib-api-routes/v1/features/list-features")
+          );
+        }
+
+        // dispatch(
+        //   openToast({
+        //     type: "success",
+        //     message: "Support Feature has been deleted successfully",
+        //   })
+        // );
+      } catch (error) {
+        console.log(error);
+        // dispatch(
+        //   openToast({
+        //     type: "error",
+        //     message: ` Error deleting Feature with error details: ${error.response.data.message}`,
+        //   })
+        // );
+      } finally {
+        setShowDeletePopup(false);
+      }
+    }
+  };
+  // console.log("getSingleRole", role);
+
+  const Headings = {
+    tableHeadings: ["S/N", "Name", "Price", "Status", "Action"],
+  };
+  return (
+    <>
+      <div>
+        <div className="flex items-center justify-between">
+          <div className=" py-2 pl-2">
+            <h1 className="text-lg font-semibold text-gray-900 ">Feature</h1>
+            <p className="text-gray-600 text-sm font-normal">
+              Manage list of the features available
+            </p>
+          </div>
+          <Button
+            onClick={() => setOpenRoleModal(true)}
+            size="sm"
+            leftIcon={<PlusIcon />}
+          >
+            Add New Feature
+          </Button>
+        </div>
+      </div>
+
+      <>
+        {loading ? (
+          <div className="min-h-[200px] flex items-center justify-center">
+            <LoadingSpinner />
+          </div>
+        ) : error ? (
+          <ErrorStatus
+            message={JSON.stringify(error?.message)}
+            statusCode={error?.status_code || error?.status}
+            link="/dashboard"
+            reload
+          />
+        ) : (
+          <div className="relative overflow-x-auto shadow sm:rounded-lg mt-6 ">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-600 ">
+              <thead className="text-xs text-gray-700 bg-gray-100 ">
+                <tr className="font-Inter">
+                  {Headings.tableHeadings.map((heading, headingIndex) => (
+                    <th
+                      key={headingIndex}
+                      scope="col"
+                      className="px-6 py-4 whitespace-nowrap uppercase font-Inter"
+                    >
+                      {heading}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+
+              {features?.data?.length === 0 ? (
+                <tbody>
+                  <tr>
+                    <td colSpan={100} className="">
+                      <div className="flex gap-5 flex-col items-center justify-center py-40">
+                        {/* <img src={AssignEmpty} alt="" /> */}
+
+                        <p className="text-2xl font-medium font-gray-600">
+                          No Feature Found
+                        </p>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              ) : (
+                <tbody>
+                  {features?.data?.map((item, rowIndex) => (
+                    <tr
+                      key={rowIndex}
+                      className="bg-white border-b text-gray-400  hover:bg-gray-50 [&_tr:last-child]:border-0 font-Inter"
+                    >
+                      <th
+                        scope="row"
+                        className="px-6 py-4 font-medium whitespace-nowrap "
+                      >
+                        {rowIndex + 1}
+                      </th>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        {/* {item?.name === "" ? "Not Available" : item?.name} */}
+                        <div className="font-normal  text-gray-600 capitalize ">
+                          {item?.name}
+                        </div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        ₦{formatNumber(item?.price)}
+                      </td>
+
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        {/* {formatDateTime(item?.account_type)} */}
+                        {item.is_active === null ? (
+                          "None"
+                        ) : (
+                          <Badge
+                            rounded
+                            className=" capitalize"
+                            // leftIcon={<DotIcon />}
+                            color={
+                              item?.is_active === true ? "success" : "warning"
+                            }
+                            text={
+                              item?.is_active === true ? "Active" : "Not-Active"
+                            }
+                          ></Badge>
+                        )}
+                      </td>
+                      {/* <td className="px-6 py-4 whitespace-nowrap">
+                        {formatDateTime(item?.created)}
+                      </td> */}
+
+                      <td
+                        // onClick={() => checkUserDetail(item)}
+                        className="px-6 py-4 whitespace-nowrap "
+                      >
+                        <div className=" flex items-center gap-2">
+                          <div
+                            className="cursor-pointer "
+                            onClick={() => fetchViewRole(item?.uuid)}
+                          >
+                            <EyeIconBold />
+                          </div>
+                          <div
+                            className="cursor-pointer "
+                            onClick={() => fetchSingleRole(item?.uuid)}
+                          >
+                            <EditIcon />
+                          </div>
+                          <div
+                            className="cursor-pointer "
+                            onClick={() => deleteFeatureData(item?.uuid)}
+                          >
+                            <DeleteIcon className="text-red-600 w-6 h-6" />
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              )}
+            </table>
+          </div>
+        )}
+
+        <div className="py-5">
+          {features.length !== 0 && (
+            <Pagination meta={features.meta} handlePaginate={handlePaginate} />
+          )}
+        </div>
+      </>
+      {openRoleModal && <AddNewFeatureModal setOpenModal={setOpenRoleModal} />}
+
+      {viewSupportModal && (
+        <div>
+          {loading ? (
+            <div className="min-h-[400px] flex items-center justify-center">
+              {/* <Spinner /> */}
+              ...
+            </div>
+          ) : (
+            <ViewFeatureModal
+              setOpenModal={setViewSupportModal}
+              featureData={feature?.data}
+            />
+          )}
+        </div>
+      )}
+
+      {editSupportModal && (
+        <div>
+          {loading ? (
+            <div className="min-h-[400px] flex items-center justify-center">
+              {/* <Spinner /> */}
+              ...
+            </div>
+          ) : (
+            <EditFeatureModal
+              setOpenModal={setEditSupportModal}
+              featureData={feature?.data}
+            />
+          )}
+        </div>
+      )}
+
+      {showDeletePopup && (
+        <>
+          <p>Coming</p>
+          <Notification
+            message="Are you sure you want to delete this feature?"
+            type="warning"
+            onCancel={() => setShowDeletePopup(false)}
+            onApprove={() => {
+              handleFeatureDelete(feature?.data);
+              setShowDeletePopup(false);
+            }}
+          />
+        </>
+      )}
+    </>
+  );
+};
+
+export default RoomFeatures;
