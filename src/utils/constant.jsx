b58b37719fd7cbee79f03311f704d126a487c69c
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2024-11-09 21:01:24 +0100

    Book a crib new updates

--- src/utils/constant.jsx
+++ src/utils/constant.jsx
@@ -28,6 +28,24 @@ export function formatDate(dateString) {
   return `${month} ${day}, ${year}`;
 }
 
+export function formattedDate(dateString) {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-based
+  const day = String(date.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+}
+
+export function formateCheckDate(dateString) {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-based
+  const day = String(date.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+}
+
 export function getNameInitials(name) {
   const words = name.split(" ");
   const initials = words.map((word) => word[0]);
