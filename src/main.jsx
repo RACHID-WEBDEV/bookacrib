dcc2df0cd263b051b60986c62db019f28750d51e
Adeyemo Rasheed <rasheedolamide2015@gmail.com>
2025-05-03 22:21:05 +0100

    Property Create and Edit Feature

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 // import { StrictMode } from "react";
 import { PersistGate } from "redux-persist/integration/react";
 // import { persistStore } from "redux-persist";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
@@ -23,11 +25,13 @@ createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <Provider store={store}>
     <PersistGate persistor={persistor}>
-      <BrowserRouter>
-        <ScrollToTop />
-        <App />
-        <Toaster />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <ScrollToTop />
+          <App />
+          <Toaster />
+        </BrowserRouter>
+      </QueryClientProvider>
     </PersistGate>
   </Provider>
   // </StrictMode>
