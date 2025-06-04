c9df8c92c2e8f693ff763b0cce11c3d38e95f13a
Adeyemo Rasheed <rasheedolamide2015@gmail.com>
2025-06-04 14:08:51 +0100

    City features

--- src/components/Section/CitiesFeatures.jsx
+++ src/components/Section/CitiesFeatures.jsx
@@ -1,7 +1,82 @@
-import citiesData from "../../data/cities";
+/* eslint-disable no-unused-vars */
+// import citiesData from "../../data/cities";
+
+import PRODUCTIMAGE1 from "src/assets/images/Rooms/city-1.png";
+import PRODUCTIMAGE2 from "src/assets/images/Rooms/city-2.png";
+import PRODUCTIMAGE3 from "src/assets/images/Rooms/city-3.png";
+import PRODUCTIMAGE4 from "src/assets/images/Rooms/city-4.png";
+
+import { FetchLocations } from "../../Hooks/useFetchLocation";
 import CityCard from "../ui/CityCard";
 
 const CitiesFeatures = () => {
+  const {
+    countries,
+    states,
+    // cities,
+    selectedCountry,
+    setSelectedCountry,
+    selectedState,
+    setSelectedState,
+    // selectedCities,
+    // setSelectedCities,
+    // loading,
+    // error,
+  } = FetchLocations();
+  // console.log("statates", states?.data?.states);
+
+  const findLagos = states?.data?.states?.find(
+    (state) => state.name === "Lagos"
+  );
+
+  const findCalabar = states?.data?.states?.find(
+    (state) => state.name === "Cross River"
+  );
+  const findKano = states?.data?.states?.find((state) => state.name === "Kano");
+  // console.log("findKano", findKano);
+
+  const citiesData = [
+    {
+      city: findLagos,
+      title: "Lagos Island ",
+      image: PRODUCTIMAGE1,
+      price: "20,000",
+      category: "Lagos State, Nigeria ",
+      discount: "20% Discount",
+      description:
+        "Fashion is a form of self-expression and autonomy at a particular period and place and in a specific context, of clothing, footwear, lifestyle, accessories, makeup, hairstyle, and body posture.",
+    },
+    {
+      city: findCalabar,
+      title: "Calabar City ",
+      image: PRODUCTIMAGE2,
+      price: "10,000",
+      category: "Cross-River State, Nigeria ",
+      discount: "",
+      description:
+        "Fashion is a form of self-expression and autonomy at a particular period and place and in a specific context, of clothing, footwear, lifestyle, accessories, makeup, hairstyle, and body posture.",
+    },
+    {
+      city: findKano,
+      title: "Kano City",
+      image: PRODUCTIMAGE3,
+      price: "10,000",
+      category: "Kano State, Nigeria ",
+      discount: "",
+      description:
+        "Fashion is a form of self-expression and autonomy at a particular period and place and in a specific context, of clothing, footwear, lifestyle, accessories, makeup, hairstyle, and body posture.",
+    },
+    {
+      city: findLagos,
+      title: "Lagos City",
+      image: PRODUCTIMAGE4,
+      price: "10,000",
+      category: "Lagos State, Nigeria ",
+      discount: "30% Discount",
+      description:
+        "Fashion is a form of self-expression and autonomy at a particular period and place and in a specific context, of clothing, footwear, lifestyle, accessories, makeup, hairstyle, and body posture.",
+    },
+  ];
   return (
     <div className="px-4 max-w-7xl mx-auto pt-6 mt-12">
       <div className=" relative flex items-center justify-between mb-4  text-neutral-800 dark:text-neutral-50">
@@ -48,7 +123,7 @@ const CitiesFeatures = () => {
       <div className=" grid grid-cols-1 md:grid-cols-2 gap-6  ">
         {citiesData
           .slice(0, 6)
-          .map(({ image, title, discount, category, price }, index) => (
+          .map(({ image, title, discount, category, price, city }, index) => (
             <CityCard
               key={index}
               image={image}
@@ -56,6 +131,7 @@ const CitiesFeatures = () => {
               discount={discount}
               category={category}
               price={price}
+              city={city}
             />
           ))}
       </div>
