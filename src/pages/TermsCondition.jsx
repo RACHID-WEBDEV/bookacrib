00a5f46cb1c71149d831995cf2bd88f33d8bb6e6
Adeyemo Rasheed <rasheedolamide2015@gmail.com>
2025-06-04 14:10:53 +0100

    Terms and Consditions

---
+++ src/pages/TermsCondition.jsx
@@ -0,0 +1,87 @@
+const TermsCondition = () => {
+  return (
+    <div className="px-4">
+      <section className="relative pt-32 py-16 dark:bg-jacarta-800 md:py-24">
+        <div className="">
+          <h1 className="text-center font-display text-2xl lg:text-4xl font-bold text-gray-700 dark:text-white">
+            Terms Of Service
+          </h1>
+          <div className="article-content mx-auto max-w-[48.125rem]">
+            <h2 className="">Introduction</h2>
+            <p className=" font-semibold">Effective Date: 1st June, 2025</p>
+            <p>
+              These Terms and Conditions govern your use of our website and
+              services. By accessing or using our platform, you agree to these
+              terms.
+            </p>
+            <h4>1. Use of the Platform</h4>
+            <ul>
+              <li>
+                Users must be at least 18 years old to register and make
+                bookings.
+              </li>
+              <li>You agree to provide accurate and current information.</li>
+            </ul>
+            <h4>2. Bookings and Payments</h4>
+            <ul>
+              <li>Bookings are not confirmed until payment is processed.</li>
+              <li>
+                Cancellation and refund policies are outlined on each property
+                listing.
+              </li>
+              <li>
+                You agree to abide by the rules of each property you book.
+              </li>
+            </ul>
+            <h4>3. User Responsibilities</h4>
+            <ul>
+              <li>
+                You are responsible for your conduct and any damage caused
+                during your stay.
+              </li>
+              <li>Reviews must be honest and not defamatory or abusive.</li>
+            </ul>
+            <h4>4. Intellectual Property</h4>
+            <p>
+              All content on www.bookacrib,com, including logos, images, and
+              software, is the property of Book A Crib or its licensors and is
+              protected by copyright.
+            </p>
+            <h4>5. Account Termination</h4>
+            <p>
+              We reserve the right to suspend or terminate accounts that violate
+              these terms or our policies.
+            </p>
+            <h4>6. Disclaimers</h4>
+            <ul>
+              <li>
+                www.bookacrib.com acts only as an intermediary between users and
+                property hosts.
+              </li>
+              <li>
+                We do not guarantee the quality or safety of properties listed.
+              </li>
+            </ul>
+
+            <h4>7. Limitation of Liability</h4>
+            <p>
+              Book A Crib is not liable for any direct, indirect, or
+              consequential damages arising from the use of our platform.
+            </p>
+            <h4>8. Governing Law</h4>
+            <p>
+              These terms are governed by the laws of Federal Republic of
+              Nigeria. Any dispute, controversy, or claim arising out of or
+              relating to this agreement shall be subject to the exclusive
+              jurisdiction of the courts of law of the Federal Republic of
+              Nigeria, and the parties hereby irrevocably submit to such
+              jurisdiction
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default TermsCondition;
